a23f95f29e3dec5e7223f4c1e4c51ca20a9650d4
Mikepa22 <102820734+Mikepa22@users.noreply.github.com>
2024-05-24 21:51:56 -0500

    Modified to fit our data
    We extracted the images from the .npz file

--- codes/tsne_plot.py
+++ codes/tsne_plot.py
@@ -3,13 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 from tensorflow.keras.models import load_model
-
+from sklearn.model_selection import train_test_split
+from numpy.random import shuffle
+import cv2
 # %% --------------------------------------- Load Models ---------------------------------------------------------------
-x_train, y_train = np.load('x_train.npy'), np.load('y_train.npy')
+# Carga tu dataset
+train_data = np.load('dataset.npz')
+images = train_data['x']
+labels = train_data['y']
+#latent=0
+# Set channel
+channel = images.shape[-1]
+
+# to 64 x 64 x channel
+real = np.ndarray(shape=(images.shape[0], 64, 64, channel))
+for i in range(images.shape[0]):
+    real[i] = cv2.resize(images[i], (64, 64)).reshape((64, 64, channel))
+
+# División del dataset en entrenamiento y validación (test)
+x_train, x_val, y_train, y_val = train_test_split(real, labels, test_size=0.3, shuffle=True, random_state=42)
+
+
 n_classes = len(np.unique(y_train))
 # encoder = load_model('bagan_encoder.h5')
-encoder = load_model('bagan_gp_encoder1.h5')
-embedding = load_model('bagan_gp_embedding1.h5')
+encoder = load_model('encoder_tennis4.h5', compile=True)
+embedding = load_model('embedding_tennis4.h5')
+encoder.compile(optimizer='adam', loss='mse')
 
 # %% --------------------------------------- TSNE Visualization --------------------------------------------------------
 def tsne_plot(encoder):
@@ -18,7 +37,7 @@ def tsne_plot(encoder):
     color = plt.get_cmap('tab10')
 
     latent = encoder.predict(x_train)  # with Encoder
-    # latent = embedding.predict([latent, y_train])  ## with Embedding model
+    #latent = embedding.predict([latent, y_train])  ## with Embedding model
     tsne_model = TSNE(n_components=2, init='random', random_state=0)
     new_values = tsne_model.fit_transform(latent)
     x = []
